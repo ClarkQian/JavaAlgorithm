c2692b12d2e72cf78d74f5895bdab35f9ec05c38
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2019-11-30 15:33:57 +0800

    添加迭代版本

--- recursion/机器人走格子.java
+++ recursion/机器人走格子.java
@@ -29,7 +29,29 @@ public class Main {
 		return solve2(x-1,y) + solve2(x,y-1);
 	}
 	
-	
+	/**
+   * 迭代形式
+   * @param m
+   * @param n
+   * @return
+   */
+  public static int solve1(int m, int n) {
+    int[][] state = new int[m + 1][n + 1];
+    for (int i = 1; i <= n; i++) {//第一行和第一列全都是1，因为只有一种走法
+      state[1][i] = 1;
+    }
+    for (int i = 1; i <= m; i++) {
+      state[i][1] = 1;
+    }
+	  //规律为：右下角等于上方和左方的值的和
+    for (int i = 2; i <= m; i++) {
+      for (int j = 2; j <= n; j++) {
+        state[i][j] = state[i][j - 1] + state[i - 1][j];
+      }
+    }
+    return state[m][n];
+  }
+
 	
 	public static void main(String[] args) {
 		solve(1, 1);
