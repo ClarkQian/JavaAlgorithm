d85e140e950754e604c2eb914a0308bf4d766bb7
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2020-04-11 00:01:29 +0800

    Create 贝祖等式递归版本.java
    特别是那个递归写得太漂亮了

---
+++ maths/贝祖等式递归版本.java
@@ -0,0 +1,27 @@
+	static long x;
+	static long y;
+
+	public static long ext_gcd(long a, long b) {
+		if (b == 0) {
+			x = 1;
+			y = 0;
+			return a;
+		}
+		long res = ext_gcd(b, a % b);
+		long x1 = x;
+		x = y;
+		y = x1 - a / b * y;
+		return res;
+
+	}
+
+	public static long linearEquation(long a, long b, long m) throws Exception {
+		long d = ext_gcd(a, b);
+		if (m % d != 0)
+			throw new Exception("no solution");
+		long n = m / d;
+		x *= n;
+		y *= n;
+		return d;
+
+	}
