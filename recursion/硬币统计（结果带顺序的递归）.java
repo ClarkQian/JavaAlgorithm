4f15487146e1d3a0af597cf72a70658c30440f38
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2020-04-13 00:52:45 +0800

    Update 硬币统计（结果带顺序的递归）.java

--- recursion/硬币统计（结果带顺序的递归）.java
+++ recursion/硬币统计（结果带顺序的递归）.java
@@ -36,7 +36,37 @@ public class Main {
 	}
 	
 
-	public static void main(String[]args){
-		System.out.println(solve(10));
+	//new one
+	public static int calc(int total, int[] coins, int n, int index) {
+		if (total < 0) {
+
+//			System.out.println("为负");
+			return 0;
+		}
+		if (total == 0) {
+//			System.out.println("正达");
+			return 1;
+		}
+
+		//一定要先检测是否达成，因为可能存在5元用了一个然后index加超过的情况。
+		if (index >= n) {
+			
+//			System.out.println("超过界限");
+			return 0;
+		}
+		// get the moderate number of index coin
+		int res = 0;
+		for (int i = 0; coins[index] * i <= total; i++) {
+//			System.out.printf("检测%d元的使用了%d个\n", coins[index], i);
+			res += calc(total - coins[index] * i, coins, n, index + 1);
+		}
+
+		return res;
+
+	}
+
+	public static void main(String[] args) {
+		int res = calc(10, new int[] { 1, 2, 5 }, 3, 0);
+		System.out.println(res);
 	}
 }
