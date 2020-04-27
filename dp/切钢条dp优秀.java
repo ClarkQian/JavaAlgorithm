a8843a116a008a264a4de4440a23ba4e7f33de41
ClarkQian <40719492+ClarkQian@users.noreply.github.com>
2020-04-27 23:17:12 +0800

    Create 切钢条dp优秀.java

---
+++ dp/切钢条dp优秀.java
@@ -0,0 +1,10 @@
+	static void dp2(int n) {
+		vs[0] = 0;
+		for (int i = 1; i <= n; i++) {// 拥有钢条的长度
+			for (int j = 1; j <= i; j++) {// 保留j为整段
+				vs[i] = max(vs[i],price[j-1]+vs[i - j]);
+			}
+		}
+
+	}
+  //将钢条分成两段，一段用之前的最优，一段用目前的价格表进行对比
